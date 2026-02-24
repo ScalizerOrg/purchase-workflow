e5bdf7a9620d1f54c78c8e0488083e531218b772
Houda <houda.bentaleb@gmail.com>
2026-02-24 19:34:37 +0000

    [FIX]:UT

--- purchase_location_by_line/tests/test_purchase_delivery.py
+++ purchase_location_by_line/tests/test_purchase_delivery.py
@@ -31,8 +31,7 @@ class TestDeliverySingle(PurchaseTestCommon):
         )
 
         # Locations
-        cls.warehouse = cls.env.ref('stock.warehouse0')
-        cls.l1 = cls.warehouse.lot_stock_id
+        cls.l1 = cls.warehouse_1.lot_stock_id
         cls.l2 = cls.env["stock.location"].create(
             {
                 "location_id": cls.l1.id,
